01450e57f165ee5607b4a6b1f87dce6e70fca3be
eduardo.saatkamp <eduardosaatkamp@gmail.com>
2024-10-12 03:02:11 -0300

    Initial commit

---
+++ imc-calculator/src/test/java/com/health/imc/imc_calculator/ImcCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.imc.imc_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
