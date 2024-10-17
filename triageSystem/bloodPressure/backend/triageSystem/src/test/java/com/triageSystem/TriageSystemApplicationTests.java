7a65a9130495d3baf43af88aa50dddba8aa0d66e
eduardo.saatkamp <eduardosaatkamp@gmail.com>
2024-10-17 01:35:40 -0300

    feat(backend): add essential files for patient triage system
    - Add pom.xml file with Maven dependencies.
    - Include the main class TriageSystemApplication.java.
    - Add configuration files like application.properties (without sensitive information).
    - Add basic tests in TriageSystemApplicationTests.java.
    - Add .gitattributes to maintain formatting consistency in the repository.

---
+++ triageSystem/bloodPressure/backend/triageSystem/src/test/java/com/triageSystem/TriageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triageSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
